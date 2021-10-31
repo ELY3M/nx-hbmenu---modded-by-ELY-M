e7ba3fb582c6978ff91be9656c2d028d12bdc01f
ELY M <elymbmx@gmail.com>
2021-10-31 07:08:50 -0500

    fixes and updates

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-export APP_VERSION	:=	3.1.1
+export APP_VERSION	:=	3.5.0-elys-mod
 
 ifeq ($(RELEASE),)
 	export APP_VERSION	:=	$(APP_VERSION)-$(shell git describe --dirty --always)
--- common/menu-list.c
+++ common/menu-list.c
@@ -35,6 +35,7 @@ static void _menuAddEntry(menu_s *m, menuEntry_s* me) {
         m->lastEntry = me;
     }
     m->xPos = 0;
+    m->slideSpeed = 0;
     m->nEntries ++;
 }
 
@@ -59,6 +60,7 @@ static void menuAddEntryToFront(menuEntry_s* me) {
         m->lastEntry = me;
     }
     m->xPos = 0;
+    m->slideSpeed = 0;
     m->nEntries ++;
 }
 
@@ -207,7 +209,7 @@ int menuScan(const char* target) {
         strncpy(me->path, tmp_path, sizeof(me->path)-1);
         me->path[sizeof(me->path)-1] = 0;
 
-        if (menuEntryLoad(me, dp->d_name, shortcut))
+        if (menuEntryLoad(me, dp->d_name, shortcut, true))
             menuAddEntry(me);
         else
             menuDeleteEntry(me, 0);
@@ -244,8 +246,23 @@ int themeMenuScan(const char* target) {
         memset(tmp_path, 0, sizeof(tmp_path));
         snprintf(tmp_path, sizeof(tmp_path)-1, "%s/%s", s_menu[!s_curMenu].dirname, dp->d_name);
 
+        bool entrytype=0;
+
+        #ifdef _DIRENT_HAVE_D_TYPE
+        if (dp->d_type == DT_UNKNOWN)
+            continue;
+        entrytype = dp->d_type != DT_REG;
+        #else
+        struct stat tmpstat;
+
+        if(stat(tmp_path, &tmpstat)==-1)
+            continue;
+
+        entrytype = (tmpstat.st_mode & S_IFMT) != S_IFREG;
+        #endif
+
         const char* ext = getExtension(dp->d_name);
-        if (strcasecmp(ext, ".cfg")==0)
+        if (entrytype || strcasecmp(ext, ".cfg")==0 || strcasecmp(ext, ".romfs")==0 || strcasecmp(ext, ".zip")==0)
             me = menuCreateEntry(ENTRY_TYPE_THEME);
 
         if (!me)
@@ -253,7 +270,7 @@ int themeMenuScan(const char* target) {
 
         strncpy(me->path, tmp_path, sizeof(me->path)-1);
         me->path[sizeof(me->path)-1] = 0;
-        if (menuEntryLoad(me, dp->d_name, shortcut))
+        if (menuEntryLoad(me, dp->d_name, shortcut, true))
             menuAddEntry(me);
         else
             menuDeleteEntry(me, 0);
@@ -265,7 +282,7 @@ int themeMenuScan(const char* target) {
     menuEntry_s* me = menuCreateEntry(ENTRY_TYPE_THEME);
 
     if(me) {
-        if(menuEntryLoad(me, textGetString(StrId_DefaultThemeName), false))//Create Default theme Menu Entry
+        if(menuEntryLoad(me, textGetString(StrId_DefaultThemeName), false, false))//Create Default theme Menu Entry
             menuAddEntryToFront(me);
         else
             menuDeleteEntry(me, 0);
